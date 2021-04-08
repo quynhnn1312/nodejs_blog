903bf54ecf3422c9a13e811090727372a4e18695
Nghiem Ngoc Quynh <nghiemquynh1312@gmail.com>
2021-04-08 21:41:45 +0700

    Refactor code create routes, app/controllers folders

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewsController {
+  // GET /news
+  index(req, res) {
+    res.render("news");
+  }
+
+  show(req, res) {
+    res.send("NEW DETAIL !!!");
+  }
+}
+
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
+const { render } = require("node-sass");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 //template engine
 app.engine(
@@ -20,13 +30,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resource/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// route
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
